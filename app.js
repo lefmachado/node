df49e8fdddfe96cf2efbe8c8080546b6ec7220b4
Leandro Machado <lefmachado@gmail.com>
2023-12-05 22:13:58 -0300

    módulo v0

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const mod1 = require('./mod1');
 const falaNome = mod1.falaNome;
 console.log(falaNome());
 
+const { Pessoa } = require('./mod1');
+
+const p1 = new Pessoa('Leandro');
+console.log(p1);
--- mod1.js
+++ mod1.js
@@ -8,3 +8,11 @@ module.exports.sobrenome = sobrenome;
 module.exports.falaNome = falaNome;
 
 console.log(module.exports);
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+
+exports.Pessoa = Pessoa;
